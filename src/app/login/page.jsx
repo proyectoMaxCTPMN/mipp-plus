5f55a4a89b9b40a81b0483e3493fb773eccad49d
daniherr <dherrera2195@gmail.com>
2025-06-02 15:55:49 -0600

    add logos, better login and change favicon

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import style from './login.module.css'
 
 export default function Login(){
@@ -5,6 +6,7 @@ export default function Login(){
     return(
         <div className={style.container}>
             <main className={style.loginBox}>
+                <Image src={"/logo-extended.svg"} width={30} height={30} alt='Logo de MIPP+' className={style.logo}/>
                 <div className={style.header}>
                     <h1 className={style.headerH1}>¡Un gusto volver a verle</h1>
                     <p className={style.headerP}>Por favor, rellene los espacios de abajo</p>
