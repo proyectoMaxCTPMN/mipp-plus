f3c09c03ab3ea4999d76ec988da00ce02afccbdb
Daniel Gutierrez <dherrera2195@gmail.com>
2025-06-16 15:36:36 -0600

    sidebar wip

--- src/app/components/nav/Navbar.jsx
+++ src/app/components/nav/Navbar.jsx
@@ -1,7 +1,45 @@
+'use client'
 
+import style from './navbar.module.css'
+import Image from 'next/image'
+import { pageIndex, setPageIndex } from '@/app/util/getPageInfo'
+import { useEffect } from 'react';
 
 export default function Navbar(){
+    let barHeight = getPageIndex() * 11.12;
+
+    useEffect({}, [])
     return(
-        <h1>Holaa</h1>
+        <div className={style.sidebarContainer}>
+            <div className={style.selectedBar} style={{top: `${barHeight}dvh`}}></div>
+            <div className={style.logo}>
+                <Image src={"/logo-short.svg"} height={20} width={20} alt='Logo' className={style.logoImage}/>
+            </div>
+
+            <div className={style.dashboard} onClick={() => setPageIndex(1)}>
+                <Image src={pageIndex == 1 ? "/menuIcons/home_selected.svg" : "/menuIcons/home_unselected.svg"} height={20} width={20} alt='Logo' className={style.iconImage}/>
+                <p className={style.subText}>Inicio</p>
+            </div>
+
+            <div className={style.history} onClick={() => setPageIndex(2)}>
+                <Image src={pageIndex == 2 ? "/menuIcons/hist_selected.svg" : "/menuIcons/hist_unselected.svg"} height={20} width={20} alt='Logo' className={style.iconImage}/>
+                <p className={style.subText}>Historial</p>
+            </div>
+
+            <div className={style.soli} onClick={() => setPageIndex(3)}>
+                <Image src={pageIndex == 3 ? "/menuIcons/soli_selected.svg" : "/menuIcons/soli_unselected.svg"} height={20} width={20} alt='Logo' className={style.iconImage}/>
+                <p className={style.subText}>Solicitud</p>
+            </div>
+
+            <div className={style.account} onClick={() => setPageIndex(4)}>
+                <Image src={pageIndex == 4 ? "/menuIcons/account_selected.svg" : "/menuIcons/account_unselected.svg"} height={20} width={20} alt='Logo' className={style.iconImage}/>
+                <p className={style.subText}>Cuenta</p>
+            </div>
+
+            <div className={style.logoff}>
+                <Image src={"/menuIcons/logoff.svg"} height={20} width={20} alt='Logo' className={style.iconImage}/>
+                <p className={style.subText} style={{color: "#940202"}}>Salir</p>
+            </div>
+        </div>
     )
 }
---
+++ src/app/util/getPageInfo.js
@@ -0,0 +1,5 @@
+export let pageIndex = 1;
+
+export function setPageIndex(index){
+    pageIndex = index;
+}
