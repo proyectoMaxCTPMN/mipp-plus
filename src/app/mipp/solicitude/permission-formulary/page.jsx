70a64c5521566aaa1669665b628e198b25940601
jondie <johndoe@example.com>
2025-07-23 16:22:52 -0600

    Dani A- Avance alejandra, el color de radio buttons account y permission-formulary - 23/7

--- src/app/mipp/solicitude/permission-formulary/page.jsx
+++ src/app/mipp/solicitude/permission-formulary/page.jsx
@@ -3,18 +3,30 @@
 import Navbar from '../../components/nav/Navbar'
 import style from './permission-formulary.module.css'
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 export default function Dashboard(){
 const [fecha, setFecha] = useState('');
+const inputRef = useRef(null);
 useEffect(() => {
-        const hoy = new Date();
+        const hoy = new Date(); // Obtener la fecha actual y Sumar 3 días a la fecha actual
         hoy.setDate(hoy.getDate() + 3);
         const yyyy = hoy.getFullYear();
         const mm = String(hoy.getMonth() + 1).padStart(2, '0');
         const dd = String(hoy.getDate()).padStart(2, '0');
         setFecha(`${yyyy}-${mm}-${dd}`);
     }, []);
+
+    const handleCalendarClick = () => {
+        if (inputRef.current) {
+            if (inputRef.current.showPicker) {
+                inputRef.current.showPicker(); // Navegadores modernos
+            } else {
+                inputRef.current.focus(); // Fallback
+            }
+        }
+    };
+
     return(
         <div className={style.container}>
             <div className={style.cardname}>
@@ -29,12 +41,12 @@ useEffect(() => {
                         <div className={style.form_row}>
                             <div className={style.inputdate_container}>
                                 <label>PERMISO PARA AUSENTARSE EN LA FECHA:</label>
-                                <input type="date" name="" id="" value={fecha} />
-                                <span><Image src={"/calendar-regular.svg"} width={20} height={20} alt='Calendar' className={style.inputdate_calendar}></Image></span>
+                                <input type="date" name="" id="" defaultValue={fecha} ref={inputRef} min={fecha}/>
+                                <span onClick={handleCalendarClick}><Image src={"/calendar-regular.svg"} width={20} height={20} alt='Calendar' className={style.inputdate_calendar}></Image></span>
                             </div>
                             <div className={style.inputradio_container}>
                                 <label>
-                                    <input type="radio" name="jornada" defaultChecked />
+                                    <input type="radio" name="jornada"/>
                                     Jornada Laboral Completa
                                 </label>
                                 <label>
