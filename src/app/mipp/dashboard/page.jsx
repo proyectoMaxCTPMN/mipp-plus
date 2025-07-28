295c30973fd2c20c726230b37c5adeb2515d987f
MainDream <dherrera2195@gmail.com>
2025-07-28 16:22:58 -0600

    working on dashboard

--- src/app/mipp/dashboard/page.jsx
+++ src/app/mipp/dashboard/page.jsx
@@ -1,12 +1,21 @@
-'use client'
+import { getCurrentUser } from '@/app/utils/auth'
 import style from './dashboard.module.css'
+import { getFullName } from '@/app/utils/userInfo';
+import RecentHistory from './RecentHistory';
 
 
-export default function Dashboard(){
-    
+export default async function Dashboard(){
+    const userId = await getCurrentUser();
+    const fullName = await getFullName(userId);
+
+
     return(
          <div className={style.container}>
-            
+            <div className={style.dashboardContainer}>
+                <h1>Inicio</h1>
+                <h1>Bienvenid@ {fullName}</h1>
+                <RecentHistory />
+            </div>
          </div>
     )
 }
---
+++ src/app/utils/userInfo.js
@@ -0,0 +1,83 @@
+"use server"
+
+import { cookies } from "next/headers"
+import { createClient } from './supabase/server'
+
+async function createSupabase(){
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+  return supabase
+}
+
+export async function getFullName(userId) {
+    const supabase = await createSupabase()
+
+    let { data: user, error } = await supabase
+    .from('users')
+    .select('first_name, last_name, second_last_name')
+    .eq('identification', userId)
+    
+    if (error) {
+      console.log(JSON.stringify(data))
+      console.error("No se pudo obtener el registro" + JSON.stringify(error))
+      return
+    }
+
+    return user[0].first_name + " " + user[0].last_name + " " + user[0].second_last_name
+}
+
+export async function getPosition(userId) {
+    const supabase = await createSupabase()
+
+    let { data: user, error } = await supabase
+    .from('users')
+    .select('positions(position)')
+    .eq('identification', userId)
+
+    const position= user[0].positions.position
+    
+    if (error) {
+      console.log(JSON.stringify(data))
+      console.error("No se pudo obtener el registro" + JSON.stringify(error))
+      return
+    }
+
+    return position[0]
+}
+
+export async function getAllInfo(userId) {
+    const supabase = await createSupabase()
+
+    let { data: user, error } = await supabase
+    .from('users')
+    .select(`
+        first_name,
+        last_name,
+        second_last_name,
+        email,
+        phone,
+        positions (position),
+        titles (title),
+        has_ownership
+    `)
+    .eq('identification', userId)
+
+    const data = {
+        first_name: user[0].first_name,
+        last_name: user[0].last_name,
+        second_last_name: user[0].second_last_name,
+        email: user[0].email,
+        phone: user[0].phone,
+        position: user[0].positions.position,
+        title: user[0].titles.title,
+        has_ownership: user[0].has_ownership
+    }
+    
+    if (error) {
+      console.log(JSON.stringify(data))
+      console.error("No se pudo obtener el registro" + JSON.stringify(error))
+      return
+    }
+
+    return data
+}
