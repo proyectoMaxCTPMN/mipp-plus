93649a37a2208f456075f60531c998a791c0c5b4
Daniel Gutierrez <dherrera2195@gmail.com>
2025-07-31 16:06:23 -0600

    error en getTitle

--- src/app/mipp/dashboard/page.jsx
+++ src/app/mipp/dashboard/page.jsx
@@ -1,12 +1,13 @@
 import { getCurrentUser } from '@/app/utils/auth'
 import style from './dashboard.module.css'
-import { getFullName } from '@/app/utils/userInfo';
+import { getFullName, getTitle } from '@/app/utils/userInfo';
 import RecentHistory from './RecentHistory';
 
 
 export default async function Dashboard(){
     const userId = await getCurrentUser();
     const fullName = await getFullName(userId);
+    const title = await getTitle(userId)
 
 
     return(
