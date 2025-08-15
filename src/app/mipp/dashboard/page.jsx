b0b17be61a02a8f2b6d31fe1e1644e85e079bef6
Happy <daniel123arg@hotmail.com>
2025-08-15 17:26:34 -0600

    Papa

--- src/app/mipp/dashboard/page.jsx
+++ src/app/mipp/dashboard/page.jsx
@@ -5,7 +5,7 @@ import { getUserFullName } from '@/app/utils/userFetch';
 
 export default async function Dashboard(){
     const userId = await getCurrentUser();
-    const fullName = await getFullName(userId);
+    const fullName = await getUserFullName(userId);
 
 
     return(
