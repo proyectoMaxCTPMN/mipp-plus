7351238e21fc765e5f3e8c5b32af57aac20df863
Daniel Gutierrez <dherrera2195@gmail.com>
2025-08-24 15:24:24 -0600

    quitando cosas aburridas

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,8 @@ export default withSentryConfig(nextConfig, {
 
   project: "javascript-nextjs",
 
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
